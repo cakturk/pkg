f4051286c64677b290c047cd83752a483b4c1ba1
Cihangir Akturk <cakturk@gmail.com>
2019-06-09 16:36:02 +0300

    cmd: example wav cropper program

---
+++ cmd/wavtrim/wavtrim.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"log"
+	"os"
+
+	"github.com/cakturk/pkg/wavtrimmer"
+)
+
+var (
+	start   = flag.Duration("start", 0, "indicates the starting point")
+	end     = flag.Duration("end", -1, "indicates where to stop")
+	inFile  = flag.String("i", "", "input wav FILE")
+	outFile = flag.String("o", "", "put newly cropped portion into this FILE: - indicates stdout")
+)
+
+func run() error {
+	var err error
+	flag.Parse()
+	if *inFile == "" {
+		return errors.New("no input file specified")
+	}
+	inf, err := os.Open(*inFile)
+	if err != nil {
+		return err
+	}
+	ouf := os.Stdout
+	if *outFile != "" && *outFile != "-" {
+		ouf, err = os.Open(*inFile)
+		if err != nil {
+			return err
+		}
+	}
+	return wavtrimmer.Trim(inf, *start, *end, ouf)
+}
+
+func main() {
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
